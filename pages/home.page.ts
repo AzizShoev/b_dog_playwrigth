93ab7172452d6431dbb3da9180e8bce23bdf01c2
Aziz Shoev <slycat1795@gmail.com>
2024-08-28 17:40:40 +0300

    fix locators

--- pages/home.page.ts
+++ pages/home.page.ts
@@ -16,11 +16,11 @@ export class HomePage {
  
  //public energy =  this.page.frameLocator('iframe[title="stage_vnqwoeivnq_bot Web App"]').first().locator('div').filter({ hasText: /^\d+\/\d+$/ }).first().innerText();
  
- public energy = this.page.frameLocator('iframe[title="stage_vnqwoeivnq_bot Web App"]').locator('#app main div card p')
+ public energy = this.page.frameLocator('iframe[title="stage_vnqwoeivnq_bot Web App"]').locator('#app main section > div:nth-child(3) p').first()
  
  public leaderBoardButton =  this.page.frameLocator('iframe[title="stage_vnqwoeivnq_bot Web App"]').getByRole('link', { name: 'Go to Leaderboard' })
  
- public levels = this.page.frameLocator('iframe[title="stage_vnqwoeivnq_bot Web App"]').locator('section div div p span')
+ public levels = this.page.frameLocator('iframe[title="stage_vnqwoeivnq_bot Web App"]').locator('#app section div div p span')
 
  public claimRewardButton =  this.page.frameLocator('iframe[title="stage_vnqwoeivnq_bot Web App"]').getByRole('button', { name: 'Сlaim your reward' })
  
@@ -101,18 +101,19 @@ else{console.log('reward window is not disaplyed')}
   }
 
   async getLevelText() {
-    return await this.levels.innerText()
-  }
+    return await this.levels.innerText();
+}
 
-  async getCurrentLevel() {
+async getCurrentLevel() {
     const levelText = await this.getLevelText();
-    const [curentLevel, maxLevel] = levelText.split('/');
-     return parseInt(curentLevel);
-  }
+    const [currentLevel, maxLevel] = levelText.split('/');
+    return parseInt(currentLevel);
+}
 
-  async getMaxLevel() {
+async getMaxLevel() {
     const levelText = await this.getLevelText();
-    const [curentLevel, maxLevel] = levelText.split('/');
-     return parseInt(maxLevel);
-  }
+    const [currentLevel, maxLevel] = levelText.split('/');
+    return parseInt(maxLevel);
+}
+
 }
