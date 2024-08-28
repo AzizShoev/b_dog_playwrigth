0dada0f78b0bf074fb4f9ad7c6808e446f6bcde1
Aziz Shoev <slycat1795@gmail.com>
2024-08-28 22:18:08 +0300

    add tests level

--- tests/homePage/levels.spec.ts
+++ tests/homePage/levels.spec.ts
@@ -1,16 +1,8 @@
 import { test, expect } from '@playwright/test';
 import { HomePage } from '../../pages/home.page';
 import { TelegramPage } from '../../pages/telegram.page';
-import { NavigationMenu } from '../../pages/navigation.page';
-import { FriendsPage } from '../../pages/friends.page';
-import { EarnPage } from '../../pages/earn.page';
-import { TasksPage } from '../../pages/tasks.page';
-import { LeaderboardPage } from '../../pages/leaderboard.page';
-import { SettingsPage } from '../../pages/settings.page';
 import { TestHelper } from '../../helpers/helper';
-import { assert } from 'console';
-import { beforeEach } from 'node:test';
-import { Fixtures } from '@playwright/test';
+import { LeaderboardPage } from '../../pages/leaderboard.page';
 
 test.use({
     storageState: 'LoginAuth2.json'
@@ -25,4 +17,52 @@ test.beforeEach(async ({ page }) => {
     await page.waitForTimeout(8000);
     await tg.checkErrorMessage();
     await help.refresh();
+});
+
+test ('Check default level', async ({ page }) => {
+    const home  = new HomePage (page);
+    const tg  = new TelegramPage (page);
+    const board = new LeaderboardPage (page);
+    
+    await tg.pressPlay();
+    await tg.pressConfirm();
+    await page.waitForTimeout(10000);
+    console.log('current level:', await home.getCurrentLevel());
+    console.log('max level:', await home.getMaxLevel());
+    expect(await home.getCurrentLevel()).toBe(1);
+    expect(await home.getMaxLevel()).toBe(10);
+    await home.goBLeaderBoard();
+    await expect (board.levelNameLink).toHaveText('Puppy Doge');
+});
+
+test ('Up to level 3', async ({ page }) => {
+    const home  = new HomePage (page);
+    const tg  = new TelegramPage (page);
+    const help = new TestHelper(page);
+    const board = new LeaderboardPage (page);
+    
+    await help.upLevel(2);
+    await tg.pressPlay();
+    await tg.pressConfirm();
+    await page.waitForTimeout(5000);
+    expect(await home.getCurrentLevel()).toBe(3);
+    expect(await home.getMaxLevel()).toBe(10);
+    await home.goBLeaderBoard();
+    await expect (board.levelNameLink).toHaveText('Bark Doge');
+});
+
+test ('Up to max level', async ({ page }) => {
+    const home  = new HomePage (page);
+    const tg  = new TelegramPage (page);
+    const help = new TestHelper(page);
+    const board = new LeaderboardPage (page);
+    
+    await help.upLevel(9);
+    await tg.pressPlay();
+    await tg.pressConfirm();
+    await page.waitForTimeout(5000);
+    expect(await home.getCurrentLevel()).toBe(10);
+    expect(await home.getMaxLevel()).toBe(10);
+    await home.goBLeaderBoard();
+    await expect (board.levelNameLink).toHaveText('Supreme Doge');
 });
