3d9bc308ae7d9b63dd3db6489762217ad37badd8
Aziz Shoev <slycat1795@gmail.com>
2024-09-02 21:00:38 +0300

    add tests

--- tests/earnPage/buyCards.spec.ts
+++ tests/earnPage/buyCards.spec.ts
@@ -37,13 +37,79 @@ test ('Buy card', async ({ page }) => {
     await page.waitForTimeout(5000);
     expect(await home.getBalance()).toEqual(10000);
     await nav.goEarn();
-    await (await earnHelp.checkCard(6)).click();
-    expect(await earn.getCardNameModal()).toMatch('Crypto Launchpad');
+    await (await earnHelp.checkCard(10)).click();
+    expect(await earn.getCardNameModal()).toMatch('Staking');
     expect(await earn.getCurrentProfitModal()).toEqual(0);
-    expect(await earn.getUpProfitModal()).toEqual(700);
+    expect(await earn.getUpProfitModal()).toEqual(50);
     expect(await earn.getCostModal()).toEqual(await earn.getUpProfitModal()*10);
     await earn.buyCard();
     await nav.goBack();
+    await page.waitForTimeout(3000);
     expect(await home.getBalance()).toEqual(10000 - (await home.getProfitPerHour()*10));
-    expect(await home.getProfitPerHour()).toEqual(700);
+    expect(await home.getProfitPerHour()).toEqual(50);
+});
+
+test ('Upgrade card from level 1 to level 2', async ({ page }) => {
+    const tg  = new TelegramPage (page);
+    const earn = new EarnPage (page);
+    const nav = new NavigationMenu (page);
+    const earnHelp = new EarnHelper(page);
+    const home = new HomePage (page);
+
+    tg.topUp('6809402010', '10000');
+    tg.pressPlay();
+    tg.pressConfirm();
+    await page.waitForTimeout(5000);
+    expect(await home.getBalance()).toEqual(10000);
+    await nav.goEarn();
+    await (await earnHelp.checkCard(10)).click();
+    expect(await earn.getCardNameModal()).toMatch('Staking');
+    await earn.buyCard();
+    expect(await earnHelp.checkCardLevel(10)).toMatch('Level 1');
+    await (await earnHelp.checkCard(10)).click();
+    expect(await earn.getCurrentProfitModal()).toEqual(50);
+    expect(await earn.getUpProfitModal()).toEqual(53);
+    expect(await earn.getCostModal()).toEqual(829);
+    await earn.upgradeCard();
+    await page.waitForTimeout(2000);
+    expect(await earnHelp.checkCardLevel(10)).toMatch('Level 2');
+    expect(await earnHelp.checkCardPrice(10)).toEqual(1379);
+    await nav.goBack();
+    expect(await home.getBalance()).toEqual(8671);
+    expect(await home.getProfitPerHour()).toEqual(103);
+});
+
+test ('Buy card with insufficient balance', async ({ page }) => {
+    const tg  = new TelegramPage (page);
+    const earn = new EarnPage (page);
+    const nav = new NavigationMenu (page);
+    const earnHelp = new EarnHelper(page);
+    const home = new HomePage (page);
+
+    tg.topUp('6809402010', '10000');
+    tg.pressPlay();
+    tg.pressConfirm();
+    await page.waitForTimeout(5000);
+    expect(await home.getBalance()).toEqual(10000);
+    await nav.goEarn();
+    await (await earnHelp.checkCard(10)).click();
+    expect(await earn.getCardNameModal()).toMatch('Staking');
+    expect(await earn.getCurrentProfitModal()).toEqual(0);
+    expect(await earn.getUpProfitModal()).toEqual(50);
+    expect(await earn.getCostModal()).toEqual(await earn.getUpProfitModal()*10);
+    await earn.buyCard();
+    expect(await earn.getBalance()).toEqual(9500);
+    expect(await earnHelp.checkCardLevel(10)).toMatch('Level 1');
+    await(await earnHelp.checkCard(7)).click();
+    expect(await earn.getLowBalanceSing()).toMatch('Need 500');
+});
+
+test ('Check profit per hour for 2 hour', async ({ page }) => {
+    const tg  = new TelegramPage (page);
+    const earn = new EarnPage (page);
+    const nav = new NavigationMenu (page);
+    const earnHelp = new EarnHelper(page);
+    const home = new HomePage (page);
+
+
 });
