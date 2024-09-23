252f5e94b0e81440050572b38ab41c37d2181b3d
Aziz Shoev <slycat1795@gmail.com>
2024-09-23 15:09:11 +0300

    fix tests

--- tests/homePage/buyBoost.spec.ts
+++ tests/homePage/buyBoost.spec.ts
@@ -9,6 +9,7 @@ import { BoostsPage } from '../../pages/boosts.page';
 import { before } from 'node:test';
 import { Fixtures } from '@playwright/test';
 import { TestHelper } from '../../helpers/helper';
+import { EarnPage } from '../../pages/earn.page';
 
 test.setTimeout(160000) 
 
@@ -33,7 +34,8 @@ test('Up balance', async ({ page }) => {
 
   await tg.topUp('6809402010', '10000');
   await page.waitForTimeout(2000);
-  await help.checkPreLastTgMessage('Your balance has been topped up by 10000' );
+  expect.soft(await help.checkPreLastTgMessage()).toMatch('Your balance has been topped up by 10000')
+  expect.soft(await help.checkLastTgMessage()).toMatch('Balance topped up')
   await help.openApp();
   try {
     await expect(home.currentBalance).toHaveText('10,000');
@@ -51,9 +53,7 @@ test('Buy Multitap', async ({ page }) => {
 
   await tg.topUp('6809402010', '10000');
   await help.openApp();
-  await home.goBoost();
-  await boost.buyMultitap();
-  await boost.buy();
+  await help.buyMulitap();
   await nav.goHome();
   await page.waitForTimeout(2000);
   try {
@@ -72,9 +72,7 @@ test ('Use Multitap', async ({ page }) => {
 
   await tg.topUp('6809402010', '10000');
   await help.openApp();
-  await home.goBoost();
-  await boost.buyMultitap();
-  await boost.buy();
+  await help.buyMulitap();
   await nav.goHome();
   await page.waitForTimeout(1000);
   await home.mine();
@@ -95,13 +93,12 @@ test('Buy Energy', async ({ page }) => {
 
   await tg.topUp('6809402010', '10000');
   await help.openApp();
-  await home.goBoost();
-  await boost.buyEnergy();
-  await boost.buy();
+  await help.buyEnergy();
   await nav.goHome();
   await page.waitForTimeout(5000)
   try {
-    expect.soft(await home.getAvailableEnergy()).toBeGreaterThan(1000)
+    console.log('Availible energy: ' + await home.getAvailableEnergy())
+    expect.soft(await home.getAvailableEnergy()).toEqual(1021)
     
   } catch (error) {
     throw new Error('Available energy is not as expected');
@@ -120,12 +117,16 @@ test ('Buy multitap whit zero balance', async ({ page }) => {
   const tg  = new TelegramPage (page);
   const boost = new BoostsPage (page);
   const help = new TestHelper(page);
+  const earn = new EarnPage(page);
 
   await help.openApp();
-  await home.goBoost();
+  await nav.goEarn();
+  await earn.goBoosters();
+  await boost.goBoostersTab();
   await boost.buyMultitap();
   try {
-    expect(await boost.getLowBalanceSing()).toContain('Need 1024');
+    expect.soft(await boost.getLowBalanceSing()).toContain('Need 1024');
+    await expect.soft(boost.lowBalanceSing).toBeDisabled();
   } catch (error) {
     throw new Error('Low balance sing is not as expected');
   }
@@ -137,12 +138,16 @@ test ('Buy energy whit zero balance', async ({ page }) => {
   const tg  = new TelegramPage (page);
   const boost = new BoostsPage (page);
   const help = new TestHelper(page);
+  const earn = new EarnPage(page);
 
   await help.openApp();
-  await home.goBoost();
+  await nav.goEarn();
+  await earn.goBoosters();
+  await boost.goBoostersTab();
   await boost.buyEnergy();
   try {
-    expect(await boost.getLowBalanceSing()).toContain('Need 1024');
+    expect.soft(await boost.getLowBalanceSing()).toContain('Need 1024');
+    await expect.soft(boost.lowBalanceSing).toBeDisabled();
   } catch (error) {
     throw new Error('Low balance sing is not as expected');
   }
@@ -157,13 +162,11 @@ test ('Buy Multitap with insufficient balance', async ({ page }) => {
 
   await tg.topUp('6809402010', '2000')
   await help.openApp();
-  await home.goBoost();
-  await boost.buyMultitap();
-  await boost.buy();
-  await page.waitForTimeout(1500)
+  await help.buyMulitap();
   await boost.buyMultitap();
   try {
-    expect(await boost.getLowBalanceSing()).toContain('Need 1072');
+    expect.soft(await boost.getLowBalanceSing()).toContain('Need 1072');
+    await expect.soft(boost.lowBalanceSing).toBeDisabled();
   } catch (error) {
     throw new Error('Low balance sing is not as expected');
   }
@@ -179,13 +182,11 @@ test ('Buy Energy Limit with insufficient balance', async ({ page }) => {
 
   await tg.topUp('6809402010', '2000')
   await help.openApp();
-  await home.goBoost();
-  await boost.buyEnergy();
-  await boost.buy();
-  await page.waitForTimeout(1500)
+  await help.buyEnergy();
   await boost.buyEnergy();
   try {
-    expect(await boost.getLowBalanceSing()).toContain('Need 1072');
+    expect.soft(await boost.getLowBalanceSing()).toContain('Need 1072');
+    await expect.soft(boost.lowBalanceSing).toBeDisabled();
   } catch (error) {
     throw new Error('Low balance sing is not as expected');
   }
@@ -201,10 +202,7 @@ test ('Change multitap level 1 to level 2', async ({ page }) => {
 
   await tg.topUp('6809402010', '2000')
   await help.openApp();
-  await home.goBoost();
-  await boost.buyMultitap();
-  await boost.buy();
-  await page.waitForTimeout(2000)
+  await help.buyMulitap();
   try {
     expect(await boost.getMultitapLevel()).toContain('Level 2');
   } catch (error) {
@@ -221,10 +219,7 @@ test ('Change energy level 1 to level 2', async ({ page }) => {
 
   await tg.topUp('6809402010', '2000')
   await help.openApp();
-  await home.goBoost();
-  await boost.buyEnergy();
-  await boost.buy();
-  await page.waitForTimeout(2000)
+  await help.buyEnergy();
   try {
     expect(await boost.getEnergyLevel()).toContain('Level 2');
   } catch (error) {
@@ -241,10 +236,7 @@ test ('Change multitap price after buy', async ({ page }) => {
 
   await tg.topUp('6809402010', '2000')
   await help.openApp();
-  await home.goBoost();
-  await boost.buyMultitap();
-  await boost.buy();
-  await page.waitForTimeout(2000)
+  await help.buyMulitap();
   try {
     expect(await boost.getMultitapPrice()).toEqual(2048);
   } catch (error) {
@@ -261,13 +253,64 @@ test ('Change energy price after buy', async ({ page }) => {
 
   await tg.topUp('6809402010', '2000')
   await help.openApp();
-  await home.goBoost();
-  await boost.buyEnergy();
-  await boost.buy();
-  await page.waitForTimeout(2000)
+  await help.buyEnergy();
   try {
     expect(await boost.getEnergyPrice()).toEqual(2048);
   } catch (error) {
     throw new Error('Price is not as expected');
   }
 });
+
+test ('Use full energy daily booster ', async ({ page }) => {
+  const tg  = new TelegramPage (page);
+  const boost = new BoostsPage (page);
+  const help = new TestHelper(page);
+  const nav  = new NavigationMenu (page);
+  const earn = new EarnPage (page);
+  const home  = new HomePage (page);
+
+  await tg.topUp('6809402010', '300000')
+  await help.openApp();
+  await help.mineTap(625)
+  await nav.goEarn();
+  await earn.goBoosters();
+  await boost.fullEnergyGetButton.click();
+  await boost.useFreeBoostButton.click();
+  await nav.goBack();
+  await page.waitForTimeout(1000)
+  console.log('Availible energy: ' + await home.getAvailableEnergy());
+  try {
+    expect(await home.getAvailableEnergy()).toEqual(2500);
+  } catch (error) {
+    new Error('Availible energy is not as expected');
+  }
+});
+
+test ('Use full energy daily booster second time', async ({ page }) => {
+  const tg  = new TelegramPage (page);
+  const boost = new BoostsPage (page);
+  const help = new TestHelper(page);
+  const nav  = new NavigationMenu (page);
+  const earn = new EarnPage (page);
+  const home  = new HomePage (page);
+
+  await tg.topUp('6809402010', '300000')
+  await help.openApp();
+  await nav.goEarn();
+  await earn.goBoosters();
+  await boost.fullEnergyGetButton.click();
+  await boost.useFreeBoostButton.click();
+  await page.waitForTimeout(1000);
+    try {
+      expect.soft(await boost.fullEnergyGetButton.isDisabled()).toBeTruthy();
+    } catch (error) {
+      new Error('Get button is active');
+    }
+  await boost.fullEnergyBoost.click();
+  try {
+    expect.soft(await boost.useFreeBoostButton.isDisabled()).toBeTruthy();
+    expect.soft(await boost.dailyTaskLvl1.locator('div:nth-child(3)')).toHaveText('0/1 full energy');
+  } catch (error) {
+    new Error('Use free energy button is active or first task is not as expected');
+  }
+  });
